c96dce7a79d1e884021d216c7b6d87b86ecf9d50
Ananya227 <ananyayadav227@gmail.com>
2024-06-24 21:11:51 +0530

    First Commit

---
+++ src/main/java/bootjpa/bootjpa/BootjpaApplication.java
@@ -0,0 +1,16 @@
+package bootjpa.bootjpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootjpaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BootjpaApplication.class, args);
+	}
+
+}
+
+
+
